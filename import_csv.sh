1f80efadf9a677608bab7399f064905231339e63
vboxuser <vboxuser@docker.myguest.virtualbox.org>
2024-12-05 09:22:36 +0100

    WIP import

--- import_csv.sh
+++ import_csv.sh
@@ -1,1 +1,10 @@
-docker exec mariadb mariadb -uroot -p"root" dolibar_database < ""
+docker cp "data/data.csv" "mariadb:/tmp/data.csv"
+docker exec mariadb mariadb -uroot -p"root" dolidb <<EOF
+use dolidb;
+load data infile "/tmp/data.csv"
+into table llx_societe
+fields terminated by 'y'
+enclosed by '"'
+lignes terminated by '\n'
+ignore 1 rows;
+EOF
