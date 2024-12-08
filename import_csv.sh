86daf3d7eae963bf6bc569cf1c78cf04d73b73d5
vboxuser <vboxuser@docker.myguest.virtualbox.org>
2024-12-08 16:42:14 +0100

    Import depuis csv fonctionnel

--- import_csv.sh
+++ import_csv.sh
@@ -1,10 +1,3 @@
 docker cp "data/data.csv" "mariadb:/tmp/data.csv"
-docker exec mariadb mariadb -uroot -p"root" dolidb <<EOF
-use dolidb;
-load data infile "/tmp/data.csv"
-into table llx_societe
-fields terminated by 'y'
-enclosed by '"'
-lignes terminated by '\n'
-ignore 1 rows;
-EOF
+temp="LOAD DATA LOCAL INFILE '/tmp/data.csv' INTO TABLE llx_societe FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\n' ignore 1 lines (@col1,@col2,@col3,@col4) set nom=@col1,status=@col2,client=@col3,fournisseur=@col4;"
+docker exec mariadb mariadb -uroot -p"root" dolidb -e "$temp"
