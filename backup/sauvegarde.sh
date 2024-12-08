cdbe1b3b92c98762ed8634bf407cae59787bd182
Valouww <160739835+Valouww@users.noreply.github.com>
2024-12-08 22:19:45 +0100

    WIP

--- backup/sauvegarde.sh
+++ backup/sauvegarde.sh
@@ -1,4 +1,4 @@
 zip -r "$(date +"%Y-%m-%d")_backup.zip" "/home/dolibarr_mariadb"
 
-# (Optionnel) Supprimer les sauvegardes plus anciennes que 7 jours
+# Supprimer les sauvegardes plus anciennes que 7 jours
 find -type f -name "backup_*.zip" -mtime +7 -exec rm -- '{}' \;
