dc161a6ed260186ff0eab541fbb4a2ecfccc0c85
Valouww <160739835+Valouww@users.noreply.github.com>
2024-12-09 23:45:53 +0100

    Correction Crontab

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ sleep 30
 bash import_csv.sh
 
 #Permet d'ajouter une ligne dans le crontab pour que tout les jours à 1H du matin le script sauvegarde.sh s'éxécute, qui fait une sauvegarde de la base de donnée
+#Attention, il faut modifier le chemin absolue pour corespondre à l'endroit du projet pour vous
 echo "0 1 * * * cd /home/vboxuser/temps/sae-dolibarr/backup/ && sudo bash sauvegarde.sh" > temp
 sudo crontab temp
 rm temp
